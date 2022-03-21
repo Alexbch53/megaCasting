b17ef32ca5a5ba9395bf2f39f046a9ba32599c3e
Alexbch53 <alexisbeucher.53@gmail.com>
2022-03-21 16:59:37 +0100

    fin de journée 21/03

--- src/Controller/CastingController.php
+++ src/Controller/CastingController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Domaine;
+use App\Entity\Metier;
 use App\Entity\OffreDeCasting;
+use App\Entity\TypeContrat;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,8 +22,20 @@ class CastingController extends AbstractController
         $OffreRepo = $em->getRepository(OffreDeCasting::class);
         $offreCasting = $OffreRepo->findAll();
 
+        $DomaineRepo = $em->getRepository(Domaine::class);
+        $Domaine = $DomaineRepo->findAll();
+
+        $TypeContratRepo = $em->getRepository(TypeContrat::class);
+        $TypeContrat = $TypeContratRepo->findAll();
+
+        $MetierRepo = $em->getRepository(Metier::class);
+        $Metier = $MetierRepo->findAll();
+
         return $this->render('mon/casting.html.twig', [
             'offreDeCasting' => $offreCasting,
+            'Domaine' => $Domaine,
+            'TypeContrat' => $TypeContrat,
+            'Metier' => $Metier,
         ]);
     }
 }
